2c2e109ca5aa3434ddb4f4644c9ea9ac3f3a7faa
gamachexx <31936427+olgam4@users.noreply.github.com>
2022-08-28 08:21:50 -0400

    🩼 move egg

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,10 @@
 import { hydrate } from 'solid-js/web'
 import { StartClient } from 'solid-start/entry-client'
 
+console.log(`
+<!--       _          \n
+       .__(.)< (MEOW) \n
+        \___)         \n
+~~~~~~~~~~~~~~~~~~--> 
+`)
 hydrate(() => <StartClient />, document)
