b444314df35a71d8dba1744b36114ca4e2517831
gamachexx <31936427+olgam4@users.noreply.github.com>
2022-08-28 08:33:21 -0400

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -29,7 +29,7 @@ export default function Root() {
                 <FileRoutes />
               </Routes>
               <div class="absolute bottom-1 left-1/2 -translate-x-1/2 text-gray-400 text-sm">
-                built with ❤️ using <u><a href="https://bat.glo.quebec">bat</a></u>
+                built with ❤️ using <u><a href="https://bat.glo.quebec">🦇</a></u>
               </div>
             </ThemeProvider>
           </Suspense>
