297bf96bae7d925355c2b30832b360f25f9daf0a
Olivier Gamache <olivier@glo.quebec>
2022-09-13 13:52:18 -0400

    🦇 hover animation

--- src/components/image/index.tsx
+++ src/components/image/index.tsx
@@ -1,9 +1,13 @@
 interface Props {
   image: string
+  hover?: boolean
 }
 
 export default function (props: Props) {
   return (
-    <img src={props.image} width="200" />
+    <div class="relative group">
+      <div class="opacity-0 group-hover:opacity-50 transition absolute blur-xl rounded-full bg-gray-900 top-0 h-44 w-44" />
+      <img class={`${props.hover && 'hover:scale-125 transition translate-z-1'}`} src={props.image} width="200" />
+    </div>
   )
 }
