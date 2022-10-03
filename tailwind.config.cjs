13627c1e286782dc6518d961714e693510b642e8
Olivier Gamache <olivier@glo.quebec>
2022-10-03 06:31:50 -0400

    👽 import every carbon icon and delete unused

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,24 @@
 const { Icons } = require('tailwindcss-plugin-icons')
 
+const carbon = require('@iconify-json/carbon/icons.json')
+
+/**
+ * @type {import('tailwindcss-plugin-icons').Options}
+ */
+const options = () => ({
+  carbon,
+})
+
+/**
+ * @type {import('tailwindcss').Config}
+ */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [
-    Icons({
-      carbon: {
-        icons: ['sun', 'moon', 'language', 'arrow-left', 'arrow-right', 'location-hazard', 'logo-github', 'moonrise', 'face-dissatisfied-filled', 'phone-voice']
-      }
-    }),
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    Icons(options),
   ],
 }
